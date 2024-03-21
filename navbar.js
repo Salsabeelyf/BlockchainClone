b443721e17ce9c38314038ae0777eb605a886940
Salsabeel Salah <salsabeelsay@gmail.com>
2024-03-21 02:20:47 +0000

    Add svg icon for dropdown menu and fix menu positioning

--- navbar.js
+++ navbar.js
@@ -19,11 +19,20 @@ $(function () {
 
   changeDropdownParent(mediaQuery);
   $("#dropdownContainer").load("dropdownMenu.html");
+  $(".dropdown").on("click", function(){
+    $(".dropdown").focus();
+  });
   $(".dropdown").on("mouseenter",function () {
-      $('#dropdownContainer').addClass('show');
-    }).on("mouseleave",function () {
-      $('#dropdownContainer').removeClass('show');
-    });
+    $('#dropdownContainer').addClass('show');
+  }).on("mouseleave",function () {
+    $('#dropdownContainer').removeClass('show');
+  });
+  $(".dropdown").on("focus",function () {
+    $('#dropdownContainer').addClass('show');
+  }).on("focusout",function () {
+    $('#dropdownContainer').removeClass('show');
+  });
+
   $('.navbar-toggler').click(function () {
     if(!$('#dropdownContainer').hasClass('showSmall')){
       $('#dropdownContainer').addClass('showSmall');
